b651fb1877cf7598366ea41ef1ef12ea39a0e2ba
SOJAN THOMAS T <123824532+SojanThomasT@users.noreply.github.com>
2023-07-27 16:16:50 +0530

    Update slot_machine.py

---
+++ slot_machine.py
@@ -0,0 +1,15 @@
+
+#defining code for deposit:
+def deposit():
+  while True:
+    amount = input("what would you like to deposit? ₹" )
+    if amount.isdigit():
+      amount = int(amount)
+      if amount > 0:
+        break
+      else:
+        print("Amount must be greater than 0. ")
+    else:
+      print("Please enter a number")
+  return amount
+    
